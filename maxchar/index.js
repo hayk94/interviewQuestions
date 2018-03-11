29d3b5acfd18c3421c2a1d653c9bc3ebc6fef60b
Hayk <hayk94@gmail.com>
2018-03-11 20:28:58 +0400

    maxChar solution

--- maxchar/index.js
+++ maxchar/index.js
@@ -7,9 +7,21 @@
 
 function maxChar (str) {
   const charMap = {}
+  let max = 0
+  let maxChar = ''
+
   for (const char of str) {
     charMap[char] = charMap[char] + 1 || 1
   }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
 }
 
 module.exports = maxChar
