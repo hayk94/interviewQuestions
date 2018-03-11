21ab63c6246dd9676540c23e56ffd136c7b46a13
Hayk <hayk94@gmail.com>
2018-03-11 20:24:14 +0400

    maxChar create charMap

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,11 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar (str) {
+  const charMap = {}
+  for (const char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+}
 
-module.exports = maxChar;
+module.exports = maxChar
